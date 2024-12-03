187f8f0f248e17204f823af59a785d59820eacc6
ujjawalbisht <ujjawal.bisht@outlook.com>
2024-12-03 23:02:03 +0530

    const, let and var keyword

--- Learning_JS/JS.js
+++ Learning_JS/JS.js
@@ -35,4 +35,36 @@ console.log(z);
 
 //  Let    const    var   keywords
 
-//  
+var i = 23 ;
+var i = 3 ;
+var i = 2 ;
+var i = 26 ;
+console.log(i)
+//  In 'var' variable can be re-declared and updated
+//    Not recommended as declaring multiple variables with same name can be confusing.
+
+let j = 23 ;
+j = 3 ;
+j = 2 ;
+j = 26 ;
+console.log(j)
+//  In 'let' variable cannot be re-declared but can be updated
+//    Mostly used in JS
+//    These variables are globally accepted.
+
+const k = 23 ;
+// In 'const' variable can neither be re-declared nor updated
+// k = 23 It will generate an error
+//    These variables are locally accepted only in {} braces
+
+
+
+let z ;
+console.log(z)
+//  This will return 'undefined' as by default output.
+
+//    const y ;
+// This will return error as const variables must initillazed.
+//    These variables are locally accepted only in {} braces
+
+
