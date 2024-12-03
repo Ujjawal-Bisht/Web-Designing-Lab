b31815f689724fc97d59cc26d131c1010f6cd545
ujjawalbisht <ujjawal.bisht@outlook.com>
2024-12-03 10:07:49 +0530

    printing 'Hello World' usig JS in console.

--- Task10_JS/JS.js
+++
@@ -1,6 +0,0 @@
-/*
-Js VALIDATION: Write JavaScript to validate the following fields of the above registration page. 
-1. Name (Name should contains alphabets and the length should not be less than 6 characters). 
-2. Password (Password should not be less than 6 characters length). 
-*/
-
