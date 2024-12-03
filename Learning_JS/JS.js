9844ca33395f6b9ec378600f7e20f3a5fba0174e
ujjawalbisht <ujjawal.bisht@outlook.com>
2024-12-03 10:17:08 +0530

    variables prac

--- Learning_JS/JS.js
+++ Learning_JS/JS.js
@@ -1,1 +1,10 @@
 console.log("Hello world");
+console.log('This will print in new line.');
+
+//  Variables
+
+a = 10
+name = 'Hello world'
+fullname = 'Hello world'
+console.log(fullname);
+console.log("fullname");
