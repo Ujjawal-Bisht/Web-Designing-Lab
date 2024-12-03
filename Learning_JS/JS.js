f399f48ccc9da16dec6c28ffd7f3737436b975cf
ujjawalbisht <ujjawal.bisht@outlook.com>
2024-12-03 10:31:35 +0530

    valiable rules

--- Learning_JS/JS.js
+++ Learning_JS/JS.js
@@ -3,8 +3,28 @@ console.log('This will print in new line.');
 
 //  Variables
 
-a = 10
-name = 'Hello world'
-fullname = 'Hello world'
+a = 10      // nuumeric
+name = 'Ujjawal'        // text
+fullname = 'Ujjawal Bisht'
 console.log(fullname);
 console.log("fullname");
+
+x = null ;
+y = undefined ;
+
+//  both null and undefined are 2 different things
+// null means this variable is empty
+// undefined means this variable contain some value but we don't know
+
+b = true ;       // boolean
+
+
+//      Java Script is dynamically typed Programming Language like Python
+z = 12;
+console.log(z);
+z = 'hello'
+console.log(z);
+
+//  Variable names are case sensitive
+// '$' dollar signs are allowed in variable names
+
