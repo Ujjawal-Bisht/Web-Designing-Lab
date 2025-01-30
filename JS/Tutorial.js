61ad77fa3ffc181d3e38f6afa6d899affa6a6fbf
ujjawalbisht <ujjawal.bisht@outlook.com>
2025-01-31 00:16:23 +0530

    new rules

--- JS/Tutorial.js
+++ JS/Tutorial.js
@@ -34,11 +34,78 @@ undefined => false
 
 // Conversion into string.
 
-let num = 33
-console.log(num);
-console.log(typeof(num))
+// let num = 33
+// console.log(num);
+// console.log(typeof(num))
 
-let numToString = String(num);
-console.log(numToString);
-console.log(typeof(numToString))
+// let numToString = String(num);
+// console.log(numToString);
+// console.log(typeof(numToString))
+
+/*
+console.log(1+2);
+console.log('1'+'2');
+console.log('1'+2);
+console.log(1+'2');
+console.log('1'+2+3);
+console.log(1+2+'3');
+
+
+console.log(true)
+console.log(false)
+
+console.log(+true);
+console.log(+false);
+console.log(+'');
+
+
+console.log(true+true);
+console.log(true+false);
+console.log(false+true);
+console.log(false+false);
+
+console.log(true+1);
+console.log(false+1);
+console.log(true+0);
+console.log(false+0);
+console.log(true-1);
+console.log(false-1);
+console.log(true-0);
+console.log(false-0);
+console.log(true-true);
+
+// console.log(true+);          Both of them will give error.
+// console.log(false+);             because they are not number.
+*/
+
+
+// console.log('1' > 2);
+// console.log('1' < '2');
+// console.log(1 < '2');
+// console.log('01' < 2);
+// console.log('01' > 2);
+
+/*
+console.log(null > 0);
+console.log(null == 0)
+console.log(null >= 0)
+
+
+console.log(undefined > 0);
+console.log(undefined == 0)
+console.log(undefined >= 0)
+
+console.log('' == 0);           //  True
+console.log('' == false);           //  True
+console.log(0 == false);            //  True
+console.log(0 == '');           //  True
+console.log(0 == null);         //  False
+console.log(0 == undefined);            //  False
+console.log(false == null);         //  False
+console.log(false == undefined);            //  False
+console.log(null == undefined);         //  True          
+*/
+
+console.log('2' == 2);      // True
+console.log('2' === 2);     // False
 
