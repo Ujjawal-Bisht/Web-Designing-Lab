28a68db6b42cf6811c26b7a589418da942538a46
ujjawalbisht <ujjawal.bisht@outlook.com>
2025-01-30 22:37:42 +0530

    LearningJS Day1 (30-01-25)

---
+++ JS/Tutorial.js
@@ -0,0 +1,44 @@
+console.log("Hello World");
+
+// Conversion into number.
+/*
+const age = '25a';
+console.log(age);
+console.log(typeof(age))
+const ageInNUM = Number(age)
+console.log(ageInNUM);
+console.log(typeof(ageInNUM))
+
+
+//Conversion into boolean .
+const num = undefined;
+console.log(num);
+
+const b = Boolean(num);
+console.log(b);
+*/
+/*
+1 => true
+0 => false
+0.1 => true
+-1 => true
+123 => true
+"" => false
+"abc" => true
+0.0 => false
+123.456 => true
+null => false
+undefined => false
+*/
+
+
+// Conversion into string.
+
+let num = 33
+console.log(num);
+console.log(typeof(num))
+
+let numToString = String(num);
+console.log(numToString);
+console.log(typeof(numToString))
+
